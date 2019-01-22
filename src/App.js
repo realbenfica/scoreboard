03e70a66696174475c9c7b93e7fe736a9299b400
“ALEX <alexgtr84@gmail.com>
2019-01-22 14:07:56 +0100

    scoreboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
+import Scoreboard from './components/Scoreboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <Title content="" />
+          <Scoreboard />
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
